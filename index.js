883292af3fc86e70326645f9a75e368f423d0691
nlok5923 <nlokhande5923@gmail.com>
2021-03-18 18:37:36 +0530

    use logger in dev env

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express=require('express');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
-const path=require('path');
-const app=express();
+const path = require('path');
+const app = express();
+const logger = require('morgan')
+
 app.use(bodyParser.json({ limit: '10mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 app.use(express.json());
+app.use(logger('dev'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname + '/../public')));
 app.use(express.static('public'));
+
 app.get("/",(req,res)=>{
     res.render('frontpage',{});
 })
