c0acba3a40951c250c0dae3fd3b39ecec40e3760
nlok5923 <nlokhande5923@gmail.com>
2021-03-18 21:26:37 +0530

    added authentication for admin

--- index.js
+++ index.js
@@ -21,6 +21,18 @@ app.get('/admin-panel', (req, res, next) => {
     res.render('admin-login', {auth:true});
 });
 
+app.post('/admin-login', (req, res, next) => {
+    var adminEmail = req.body.usremail;
+    var adminPassword = req.body.usrpsw;
+    console.log(req.body)
+    if (adminEmail === 'nlok5923@gmail.com' && adminPassword === '123') {
+      res.render('admin-panel', {});
+    } else {
+    //   var userVerified = false;
+      res.render('admin-login', {auth:false});
+    }
+});
+
 app.get('/sign-in', (req, res) => {
     res.render('sign-in', {auth:true});
 });
